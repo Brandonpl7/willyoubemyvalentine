e92f7b5d2fe92cfc4c61f89fa4e9fd1300df3145
Brandonpl7 <132606905+Brandonpl7@users.noreply.github.com>
2025-02-14 16:05:35 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jaz Mailob",
+    valentineName: "Jaz",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
